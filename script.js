86750ff8265ab51ee781af9c816be58560ab37a7
Moacir Amaro <moacirjgt@gmail.com>
2021-05-30 13:16:45 -0300

    add close function

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ const cards = document.querySelector(".cards-container");
 const modal = document.querySelector(".modal");
 const closeButton = document.querySelector(".close");
 const modalContent = document.querySelector(".modal-content");
+const loadMore = document.querySelector(".load-more");
 
-function getChars() {
+let count = 0;
+
+function getChars(count) {
   fetch(
-    `${baseUrl}/v1/public/characters?ts=${ts}&apikey=${apiKey}&hash=${hash}`
+    `${baseUrl}/v1/public/characters?orderBy=name&limit=20&offset=${count}&ts=${ts}&apikey=${apiKey}&hash=${hash}`
   )
     .then((response) => {
       if (!response.ok) {
@@ -20,37 +23,41 @@ function getChars() {
       return response.json();
     })
     .then((response) => {
+      //Aqui iremos criar os cards com os heróis
+
       let card = document.createElement("div");
       let heroIdAtt = document.createAttribute("id");
 
       let heroImage = document.createElement("img");
       let heroImageAtt = document.createAttribute("src");
       let heroName = document.createElement("h2");
-      //let heroDescription = document.createElement('p')
       let btn = document.createElement("button");
       btn.classList.add("btn");
       btn.classList.add("hero-btn");
+
+      // Depois de buscar os resultados na api, fazemos uma iteração criando
+      // o card com as informações do herói
+
       response.data.results.map((element) => {
         heroIdAtt.value = element.id;
-        btn.setAttributeNode(heroIdAtt);
+        btn.setAttributeNode(heroIdAtt); // Adicionamos o id do herói ao botão para chamar o modal
         heroImageAtt.value = `${element.thumbnail.path}.${element.thumbnail.extension}`;
         heroImage.setAttributeNode(heroImageAtt);
         heroName.innerText = element.name;
-        //heroDescription.innerText = element.description != '' ? element.description : "Hero don't have a description"
         btn.innerText = `View More about ${element.name}`;
         card.appendChild(heroImage);
         card.appendChild(heroName);
-        //card.appendChild(heroDescription)
         card.appendChild(btn);
         card.classList.add("card");
         cards.innerHTML += card.outerHTML;
       });
     })
     .then((r) => {
+      // Depois de criar os card adicionaremos um botão para abrir um modal
+
       let buttons = document.querySelectorAll(".hero-btn");
       buttons.forEach((btn) => {
         btn.addEventListener("click", () => {
-          console.log(btn.id);
           showInfo(btn.id);
           modal.classList.remove("hidden");
         });
@@ -67,6 +74,8 @@ function getChars() {
               return response.json();
             })
             .then((response) => {
+              // Depois de buscar as informações do herói pelo ID adicionaremos as informações ao modal
+
               let card = document.createElement("div");
               let heroImage = document.createElement("img");
               let heroImageAtt = document.createAttribute("src");
@@ -88,7 +97,6 @@ function getChars() {
               card.appendChild(heroName);
               card.appendChild(heroDescription);
               card.classList.add("card");
-              console.log(card);
 
               modalContent.innerHTML += card.outerHTML;
 
@@ -99,12 +107,9 @@ function getChars() {
               comicCard.appendChild(title);
 
               character.comics.items.forEach((comic) => {
-                let comicLink = document.createElement("a");
-                let comicLinkAtt = document.createAttribute("href");
+                let comicLink = document.createElement("p");
 
-                comicLinkAtt.value = comic.resourceURI;
                 comicLink.innerText = comic.name;
-                comicLink.setAttributeNode(comicLinkAtt);
                 comicCard.appendChild(comicLink);
               });
               modalContent.innerHTML += comicCard.outerHTML;
@@ -116,7 +121,17 @@ function getChars() {
 
 getChars();
 
-closeButton.addEventListener("click", () => {
-  console.log("clicou");
+function closeModal() {
   modal.classList.add("hidden");
+  modalContent.innerHTML = "";
+}
+
+document.addEventListener("keyup", (e) => {
+  if (e.key == "Escape") {
+    closeModal();
+  }
 });
+
+function moreContent() {
+  getChars((count += 20));
+}
